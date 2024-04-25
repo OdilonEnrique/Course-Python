dd52543b75d6f41bdb742f3320f6045dd891bdb0
Odilon Enrique <odilonegduarte@gmail.com>
2024-04-25 19:40:51 -0300

    add challenge name

---
+++ class-01-data-types/challenges/name.py
@@ -0,0 +1,7 @@
+# Capture o nome do usuário via "input"
+# Atribua o nome a uma variável
+# Imprima na tela uma mensagem de boas vindas com o nome 
+
+name = input("Digite o seu nome --> ")
+
+print(f"Olá {name}! Seja Bem-Vindo ao Curso de Python !!!")
